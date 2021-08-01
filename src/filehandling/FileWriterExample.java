5831a917b3aa8136d67094e57862ef7fe7b7f7f9
crestasom <crestasom@gmail.com>
2021-08-01 08:46:47 +0545

    added file reader code

--- src/filehandling/FileWriterExample.java
+++ src/filehandling/FileWriterExample.java
@@ -2,25 +2,55 @@ package filehandling;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class FileWriterExample {
-	public static void main(String[] args) {
-		FileWriter writer = null;
-		try {
-			writer = new FileWriter("demo.txt");
-			writer.write("Hello");
-			writer.write(5 / 0);
+//	public static void main(String[] args) {
+//		FileWriter writer = null;
+//		try {
+//			writer = new FileWriter("demo.txt");
+//			writer.write("Hello");
+//			writer.write(5 / 0);
+//
+//		} catch (IOException ex) {
+//
+//		} finally {
+//			try {
+//				writer.close();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//	}
 
-		} catch (IOException ex) {
+	//File writing using FileWriter
+//	public static void main(String[] args) throws Exception {
+//
+//		System.out.println("Enter your name:");
+//		Scanner sc = new Scanner(System.in);
+//		String name = sc.nextLine();
+//		FileWriter writer = new FileWriter("demo.txt", true);
+//		writer.write(name);
+//		writer.write("\n");
+//		writer.close();
+//
+//	}
+	//File writing using PrintWriter
+	public static void main(String[] args) throws Exception {
 
-		} finally {
-			try {
-				writer.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		System.out.println("Enter your name:");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		FileWriter writer = new FileWriter("c:/test/demo.txt", true);
+		PrintWriter pw=new PrintWriter(writer);
+		//writer.write(name);
+		//writer.write("\n");
+		pw.printf("The name of user is %s",name);
+		pw.close();
+		writer.close();
+
 	}
 
 	/*
