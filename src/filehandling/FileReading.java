f3d371d22c09738ab431a91497af42d88b4b4115
crestasom <crestasom@gmail.com>
2021-08-02 09:47:43 +0545

    updated file reading examples

--- src/filehandling/FileReading.java
+++ src/filehandling/FileReading.java
@@ -8,39 +8,47 @@ import java.util.Scanner;
 public class FileReading {
 //using file reader
 //	public static void main(String[] args) throws IOException {
-//		FileReader fr = new FileReader("hello.txt");
-//		int n;
+//		FileReader fr = new FileReader("c:\\test\\demo.txt");
+//
 //		// fr.read method reads char by char and returns ascii value of the read
 //		// character
 //		// it also automatically points to next character when current character is read
 //		// it returns -1 when End of File(EOF) is reached
+//		int n; // Hello World
 //		while ((n = fr.read()) != -1) {
 //			System.out.print((char) n);
+//			// n = fr.read();
 //		}
-	// fr.close();
+//		fr.close();
 //	}
 
 //	// using buffered reader class
-//	public static void main(String[] args) throws IOException {
-//		FileReader fr = new FileReader("hello.txt");
-//		BufferedReader br = new BufferedReader(fr);
-//		String s;
-//		// buffered reader is handy when we need more sophisticated method to read data
-//		while ((s = br.readLine()) != null) {
-//			System.out.println(s);
-//		}
-//		br.close();
-//		fr.close();
-//	}
-	// using Scanner class
 	public static void main(String[] args) throws IOException {
-		FileReader fr = new FileReader("hello.txt");
-		Scanner sc = new Scanner(fr);
+		FileReader fr = new FileReader("c:\\test\\numbers.txt");
+		BufferedReader br = new BufferedReader(fr);
 		String s;
 		// buffered reader is handy when we need more sophisticated method to read data
-		while (sc.hasNextLine()) {
-			System.out.println(sc.nextLine());
+		s = br.readLine();
+		String[] nums = s.split(" ");
+		for (String num : nums) {
+			int n = Integer.parseInt(num);
+			// names[i++]=s;
+			System.out.println(n);
 		}
-		sc.close();
+		
+
+		br.close();
+		fr.close();
 	}
+// using Scanner class
+//	public static void main(String[] args) throws IOException {
+//		FileReader fr = new FileReader("C:\\test\\numbers.txt");
+//		Scanner sc = new Scanner(fr);
+//		String s;
+//		while (sc.hasNextInt()) {
+//			int n = sc.nextInt();
+//			System.out.println(n);
+//		}
+//		sc.close();
+//	}
 }
